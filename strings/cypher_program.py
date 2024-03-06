95576b4b1656e50338f13fec0eb9cb0b55e5ba85
Michael R <rudmichael@outlook.com>
2024-03-06 11:32:16 -0800

    Ceaser Cyphering

--- strings/cypher_program.py
+++ strings/cypher_program.py
@@ -1,6 +1,15 @@
 '''Michael Rud, 350787156'''
 
 def ceaser_cypher(word, shift):
+
+    '''
+    The function takes word and shift variables as arguments and uses a for loop to check for all characters in word.
+    If the character is present in the alphabet - taking the digital data from ascii table of the letter a or A if char is uppercase.
+    Then ascii_offset is being subtracted from the digital data of our character and shift being added.
+    To ensure that all words are in the alphabet we are keeping it in the range of 26 (letters in the alphabet) and adding
+    ascii_offset to it. Then converting it back to a character and adding to an empty string. If the character is not in the alphabet
+    then its just being added to the empty string which returns after.
+    '''
     encrypted_word = ''
     for char in word:
         if char.isalpha():
