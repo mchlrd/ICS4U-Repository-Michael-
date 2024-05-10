c62b9766fb199eedcba9fda0a96fea12857c3e5d
Michael R <rudmichael@outlook.com>
2024-05-10 11:32:36 -0700

    removed old django prj

--- Final-Comp-Sci-Project/csf12/csf12/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.outlook.com'
-EMAIL_HOST_USER = 'csf12.login@outlook.com'
-EMAIL_HOST_PASSWORD = 'mtwtfss06Qm'
-EMAIL_PORT = 587
--- Final-Comp-Sci-Project/csf12/csf12app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Csf12AppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "csf12app"
