93d57bfad424810afaba573a9a3aaa1bb8605220
Michael R <rudmichael@outlook.com>
2024-05-14 16:20:56 -0700

    laptop changes

--- Final-Comp-Sci-Project/final_py_scripts/AICoach.py
+++ Final-Comp-Sci-Project/final_py_scripts/AICoach.py
@@ -4,7 +4,7 @@ import time
 
 import PoseModule as pm
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r'C:\Users\Michael\Documents\compsci_grade12\ICS4U-Repository-Michael-\Final-Comp-Sci-Project\aicoachmp4s\6.mp4')
 detector = pm.poseDetector()
 count = 0
 dir = 0
@@ -21,7 +21,7 @@ while True:
     if len(lmList) != 0:
         #Left arm
         angle = detector.findAngle(img, 11, 13, 15)
-        per = np.interp(angle, (80, 345), (0, 100)) #TROUBLESHOOT THIS <------------
+        per = np.interp(angle, (200, 315), (0, 100)) #TROUBLESHOOT THIS <------------
         #print(angle, per)
         
         #check for reps
@@ -36,12 +36,10 @@ while True:
                 dir = 0
 
         print(count)
-        cv2.rectangle(img, (0, 450), (250,720),(255,0,0), cv2.FILLED)
-        cv2.putText(img, f'{int(count)}', (45,670), cv2.FONT_HERSHEY_PLAIN, 15, (255,255,255), 25)
+        cv2.putText(img, f'{int(count)}', (45,670), cv2.FONT_HERSHEY_PLAIN, 12, (255,255,255), 25)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'{int(fps)}', (50,100), cv2.FONT_HERSHEY_PLAIN, 5, (255,255,255), 5)
     cv2.imshow('Image', img)
     cv2.waitKey(1)
